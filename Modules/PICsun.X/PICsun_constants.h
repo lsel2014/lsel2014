36226e31338d635e476cf3c8c39b513ed57226e2
jagonre <jagonre@gmail.com>
2014-03-25 01:51:36 +0100

    Primera version PICsun
    Proyecto de MPLABX

---
+++ Modules/PICsun.X/PICsun_constants.h
@@ -0,0 +1,26 @@
+/* 
+ * File:   PICsun_constants.h
+ * Author: Javier Gonzalez Recio
+ *
+ * Created on March 23, 2014, 11:28 PM
+ */
+
+#ifndef PICSUN_CONSTANTS_H
+#define	PICSUN_CONSTANTS_H
+
+#define DAY_SIM 60      //simulation time in seconds
+#define DAY_REAL 86400  //day duration in seconds
+
+#define SEC_SIM 1440    //day_real/day_sim
+
+#define LIGHT_SEGMENTS_N 19
+
+#define DEFAULT_SUNRISE_H 6
+#define DEFAULT_SUNRISE_M 0
+#define DEFAULT_SUNRISE_S 0
+#define DEFAULT_SUNSET_H 18
+#define DEFAULT_SUNSET_M 0
+#define DEFAULT_SUNSET_S 0
+
+#endif	/* PICSUN_CONSTANTS_H */
+
---
+++ Modules/PICsun.X/build/default/debug/PICsun_main.p1.d
@@ -0,0 +1,4 @@
+ build/default/debug/PICsun_main.d  \
+ build/default/debug/PICsun_main.p1:  \
+ PICsun_main.c  \
+PICsun_constants.h 
