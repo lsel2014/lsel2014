43daea06e31232ee21fee4e59d82b70efa410feb
jagonre <jagonre@gmail.com>
2014-03-26 22:16:08 +0100

    Update AutoTestSun.c

--- SoftwarePi/SunPruebas/AutoTestSun.c
+++ SoftwarePi/SunPruebas/AutoTestSun.c
@@ -4,11 +4,17 @@
 #include <string.h>
 #include <time.h>
 
-void testSun() {
+#define COLOR_RED     		"\x1b[31m"
+#define COLOR_GREEN		"\x1b[32m"
+#define COLOR_RESET   		"\x1b[0m"
+
+int testSun() {
 
 	srand(time(NULL));
 
 	int v1, v2, v3, v4;
+	int compare;
+	int successes, result;
 
 	v1 = (random() % 27)+1;
 	v2 = (random() % 11)+1;
@@ -31,18 +37,86 @@ void testSun() {
 		sprintf (buffer, "%d/%d/%d", v1, v2, v3);
         }
 	
-	printf ("Se va a simular la fecha: %s\n", buffer);
-
+	//printf ("Se va a simular la fecha: %s\n", buffer);
 	sun_t* sun = sun_new (buffer, "00:00:00", 1, 1);
-	printf ("Amanecer: %s \nAnochecer: %s \n", sun->sunrise, sun->sunset);
+	
+	printf ("Amanecer: %s\t->\tHoras ", sun->sunrise);
+	compare = dateToInt(sun,0,0);
+	if (compare < 0 || compare > 23) {
+		printf(COLOR_RED"NOK"COLOR_RESET);
+	} else {
+		printf(COLOR_GREEN"OK!"COLOR_RESET);
+		successes++;
+	}
+
+	printf("\tMinutos ");
+	compare = dateToInt(sun,1,0);
+	if (compare < 0 || compare > 59) {
+		printf(COLOR_RED"NOK"COLOR_RESET);
+	} else {
+		printf(COLOR_GREEN"OK!"COLOR_RESET);
+		successes++;
+	}
+
+	printf("\tSegundos ");
+	compare = dateToInt(sun,2,0);
+	if (compare < 0 || compare > 59) {
+		printf(COLOR_RED"NOK\n"COLOR_RESET);
+	} else {
+		printf(COLOR_GREEN"OK!\n"COLOR_RESET);
+		successes++;
+	}
+
+	printf ("Anochecer: %s\t->\tHoras ", sun->sunset);
+	compare = dateToInt(sun,0,1);
+	if (compare < 0 || compare > 23) {
+		printf(COLOR_RED"NOK"COLOR_RESET);
+	} else {
+		printf(COLOR_GREEN"OK!"COLOR_RESET);
+		successes++;
+	}
+
+	printf("\tMinutos ");
+	compare = dateToInt(sun,1,1);
+	if (compare < 0 || compare > 59) {
+		printf(COLOR_RED"NOK"COLOR_RESET);
+	} else {
+		printf(COLOR_GREEN"OK!"COLOR_RESET);
+		successes++;
+	}
+
+	printf("\tSegundos ");
+	compare = dateToInt(sun,2,1);
+	if (compare < 0 || compare > 59) {
+		printf(COLOR_RED"NOK\n"COLOR_RESET);
+	} else {
+		printf(COLOR_GREEN"OK!\n"COLOR_RESET);
+		successes++;
+	}
+
+	if (successess != 6) {
+		result=0;
+	} else {
+		result=1;
+	}
 
 	sun_destroy(sun);
+	return result;
 
 }
 
 int main (int argc, char** argv) {
+	
+	int r;
 
-	testSun();
+	r = testSun();
+	
+	if (r == 0) {
+		printf(COLOR_RED"TEST NOK\n"COLOR_RESET);
+	} else {
+		printf(COLOR_GREEN"TEST OK!\n"COLOR_RESET);
+	}
+	
 	return 0;
 
 }
