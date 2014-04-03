bb5b46cb831957922afa0ed56fa23d8f7e256a83
jagonre <jagonre@gmail.com>
2014-04-03 02:47:04 +0200

    Added sensors

---
+++ Model/Sensors/mainSensor.c
@@ -0,0 +1,53 @@
+/*
+ * mainSensor.c
+ *
+ *  Created on: 02/04/2014
+ *      Author: Javier
+ */
+
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <wiringPi.h>
+
+#include "sensor.h"
+
+//just for testing
+int main() {
+
+	int pin;
+
+	if (wiringPiSetup () == -1)
+			exit (1) ;
+	for (pin = 0 ; pin < 8 ; pin++)
+		pinMode (pin, INPUT);
+
+	sensor_t* s1 = sensorIR_new(3, 1, 1);
+
+	int line;
+	int flag;
+
+	line = sensorIR_get_GPIOline(s1);
+	printf("%d\n", line);
+	sensorIR_set_GPIOline(s1, 8);
+	line = sensorIR_get_GPIOline(s1);
+	printf("%d\n", line);
+
+	flag = sensorIR_get_GPIOflag(s1);
+	printf("%d\n", flag);
+	sensorIR_rise_GPIOflag(s1);
+	flag = sensorIR_get_GPIOflag(s1);
+	printf("%d\n", flag);
+	sensorIR_set_GPIOflag(s1,LOW);
+	flag = sensorIR_get_GPIOflag(s1);
+	printf("%d\n", flag);
+	sensorIR_readSensor(s1);
+	flag = sensorIR_get_GPIOflag(s1);
+	printf("%d\n", flag);
+
+
+
+
+
+	return 0;
+}
