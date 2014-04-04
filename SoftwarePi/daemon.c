70941176650be89dd8237543263022bf992b5b84
Jose Martin <jose.martin.burgos@gmail.com>
2014-04-04 11:37:32 +0200

    Daemon WIP.

--- SoftwarePi/daemon.c
+++ SoftwarePi/daemon.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
@@ -8,20 +7,18 @@
 #include <native/task.h>
 #include <native/timer.h>
 
+//Modules
+#include "trainCtrl.h"
 
 /* Move to headers */
 #define TASK_POLL_PERIOD 100000000
 #define TASK_SUN_PERIOD 2000000000
-#define TASK_SPEEDCTRL_PERIOD 500000000
 
-
-void initializeModel (void) {
+void initializeModel(void) {
 
 }
 
-
-
-void initializeXenomaiEnv (void) {
+void initializeXenomaiEnv(void) {
 	// Catch signals
 	signal(SIGTERM, catch_signal);
 	signal(SIGINT, catch_signal);
@@ -33,15 +30,13 @@ void initializeXenomaiEnv (void) {
 	rt_print_auto_init(1);
 }
 
-
 void initializeWiringPi(void) {
 	//variableInit();
-	system("gpio load i2c");
-	//pinModes ....
-}
 
-void initializePackets (void) {
-	//initIO();
+	// Replace with proper I2C modules load on kernel
+	system("gpio load i2c");
+
+	//pinModes ....
 }
 
 void daemon_poll_sensors(void *arg) {
@@ -53,21 +48,8 @@ void daemon_poll_sensors(void *arg) {
 	rt_task_set_periodic(NULL, TM_NOW, TASK_POLL_PERIOD);
 
 	while (1) {
-		rt_task_wait_period(NULL);
-		printf ("Poll!\n");
-	}
-}
-void daemon_send_trainpackets(void *arg) {
-	/*
-	 * Arguments: &task (NULL=self),
-	 * start time,
-	 * period (here: 1 s)
-	 */
-	rt_task_set_periodic(NULL, TM_NOW, TASK_SPEEDCTRL_PERIOD500000000);
-
-	while (1) {
-		rt_task_wait_period(NULL);
-		printf("Packet!\n");
+		rt_task_wait_period(NULL );
+		printf("Poll!\n");
 	}
 }
 
@@ -80,19 +62,16 @@ void daemon_update_sun(void *arg) {
 	rt_task_set_periodic(NULL, TM_NOW, TASK_SUN_PERIOD);
 
 	while (1) {
-		rt_task_wait_period(NULL);
-		printf ("Sun!\n");
+		rt_task_wait_period(NULL );
+		printf("Sun!\n");
 	}
 }
 
-
-void catch!_signal(int sig) {
+void catch_signal(int sig) {
 }
+
 int main(int argc, char* argv[]) {
-	RT_TASK task_poll, task_packets, task_sun;
-
-
-
+	RT_TASK task_poll, task_trainctrl, task_sun;
 
 	// Initialize the model
 	initializeModel();
@@ -103,30 +82,30 @@ int main(int argc, char* argv[]) {
 	// Initialize wiringPi lib, configure IO
 	initializeWiringPi();
 
-	// Initialize the communication
-	initializePackets();
+	// Initialize the train controller
+	trainCtrl_init();
 
 	/*
-	 	 * Arguments: &task,
-		 * name,
-		 * stack size (0=default),
-		 * priority,
-		 * mode (FPU, start suspended, ...)
-		 */
+	 * Arguments: &task,
+	 * name,
+	 * stack size (0=default),
+	 * priority,
+	 * mode (FPU, start suspended, ...)
+	 */
 	rt_task_create(&task_poll, "polling", 0, 50, 0);
-	rt_task_create(&task_packets, "packets", 0, 55, 0);
+	rt_task_create(&task_trainctrl, "trainCtrl", 0, 55, 0);
 	rt_task_create(&task_sun, "sun", 0, 20, 0);
 	/*
 	 * Arguments: &task,
 	 * task function,
 	 * function argument*/
-	rt_task_start(&task_poll, &daemon_poll_sensors, NULL);
-	rt_task_start(&task_packets, &daemon_send_trainpackets, NULL);
-	rt_task_start(&task_sun, &daemon_update_sun, NULL);
+	rt_task_start(&task_poll, &daemon_poll_sensors, NULL );
+	rt_task_start(&task_trainctrl, &trainCtrl_periodic, NULL );
+	rt_task_start(&task_sun, &daemon_update_sun, NULL );
 
 	pause();  //REMOVE, replace by a join to the socket thread
 	rt_task_delete(&task_poll);
-	rt_task_delete(&task_packets);
+	rt_task_delete(&task_trainctrl);
 	rt_task_delete(&task_sun);
 	return 0;
 }
