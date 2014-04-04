6a092962f3bc2c1fc8d57916cedc21b3abfc7f8b
Enrique Casado Díez <enrique.cdiez@gmail.com>
2014-04-04 21:51:03 +0200

    Alpha del intérprete

--- Model/Actuators/actuator.c
+++ Model/Actuators/actuator.c
@@ -1,4 +1,5 @@
 #include "actuator.h"
+#include "interp.h"
 
 actuator_t* actuator_new (sun_date_t date, direction_t initialRailway){
 
@@ -7,8 +8,13 @@ actuator_t* actuator_new (sun_date_t date, direction_t initialRailway){
 	this->railChange = rail_change_new(initialRailway);
 	this->trafficLight = traffic_light_new(OFF);
 	this->sun = sun_new (date, 1, 1);
+	this->frameGen = NULL;
 	pthread_mutex_init (&this->mutex, NULL);
-	//this->frameGen = NULL;
+
+	interp_addcmd ("railway", rail_change_cmd, "Changes railway (left | right)");
+	interp_addcmd ("sun", sun_cmd, "Starts a given day simulation (dd/mm/yyyy)");
+	interp_addcmd ("speed", speed_cmd, "Changes selected train's speed");
+
 	return this;
 
 }
@@ -61,10 +67,46 @@ frames_t* actuator_get_frame_gen (actuator_t* this){
 	return this->frameGen;
 }
 
+/*
+ * Commands
+ */
+
+int rail_change_cmd (char* arg){
+
+	if (0 == strcmp (arg, "left"){
+		rail_change_t* railChange = actuator_get_rail_change (actuator);
+		rail_change_set_direction(railChange, LEFT);
+		printf ("Left rail selected\n");
+		return 0;
+	}
+	if (0 == strcmp (arg, "right"){
+		rail_change_t* railChange = actuator_get_rail_change (actuator);
+		rail_change_set_direction(railChange, RIGHT);
+		printf ("Right rail selected\n");
+		return 0;
+	}
+	return 1;
+}
+
+int sun_cmd (char* arg){
+	sun_destroy (actuator->sun);
+	sun_t* sun = sun_new (arg, 1, 1);
+	actuator_set_sun (actuator, sun);
+	printf ("Simulating day: %s\n", arg);
+	return 0;
+}
+
+int speed_cmd (char* arg){
+	// Cosas de la velocidad...
+	return 0;
+}
+
+
 // Dummy main, just to test compilation and fix syntax errors
 
 int main (int argc, char** argv){
 	actuator_t* actuators = actuator_new("01/01/2001", RIGHT);
+	interp_run();
 	return 0;
 }
 	
