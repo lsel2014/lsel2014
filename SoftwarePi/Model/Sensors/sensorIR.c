85d858940b32719b43ea87bb2b9e7bbc9df73915
jagonre <jagonre@gmail.com>
2014-04-06 03:31:06 +0200

    IR sensor redone

--- SoftwarePi/Model/Sensors/sensorIR.c
+++ SoftwarePi/Model/Sensors/sensorIR.c
@@ -5,104 +5,106 @@
  *      Author: Javier
  */
 
+#include "sensorIR.h"
+
 #include <pthread.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
 #include <wiringPi.h>
 
-#include "sensor.h"
+#define COLOR_RED "\x1b[31m"
+#define COLOR_GREEN "\x1b[32m"
+#define COLOR_RESET "\x1b[0m"
 
-sensor_t*
-sensorIR_new (int GPIOline, int prio, int ceil)
+static void sensorIR_isr0 (void)
 {
-	sensor_t* this = (sensor_t*) malloc (sizeof (sensor_t));
-	sensorIR_init (this, GPIOline, prio, ceil);
+	printf(COLOR_GREEN"Detectado diesel\n"COLOR_RESET);
+	//detected diesel train, do something
+}
+
+static void sensorIR_isr1 (void)
+{
+	printf(COLOR_GREEN"Detectado renfe\n"COLOR_RESET);
+	//detected renfe train, do something
+}
+
+static void (*sensorIR_isr[]) (void) = {
+  sensorIR_isr0,
+  sensorIR_isr1,
+};
+
+sensorIR_t*
+sensorIR_new(int id, int prio, int ceil)
+{
+	sensorIR_t* this = (sensorIR_t*) malloc(sizeof(sensorIR_t));
+	sensorIR_init(this, id, prio, ceil);
 	return this;
 }
 
 void
-sensorIR_init (sensor_t* this, int GPIOline, int prio, int ceil)
+sensorIR_init(sensorIR_t* this, int id, int prio, int ceil)
 {
+	int i;
 
-	this->GPIOline = GPIOline;
-	this->GPIOflag = LOW;
-	pthread_mutex_init (&this->mutex_sensor, NULL);
+	this->id = id;
+
+	for (i = 0; i < NUMBER_OF_TRAINS; i++) {
+		this->GPIOlines[i] = (id * 2) + i;
+
+		if (wiringPiISR (this->GPIOlines[i], INT_EDGE_RISING, sensorIR_isr[i]) < 0) {
+		    fprintf (stderr, "Unable to setup ISR: %s\n", strerror (errno)) ;
+		}
+	}
+
+	pthread_mutex_init(&this->mutex_sensorIR, NULL);
 	//Tareas xenomai.
 }
 
 void
-sensorIR_destroy (sensor_t* this)
+sensorIR_destroy(sensorIR_t* this)
 {
-	if (this->GPIOline) free ((int*)this->GPIOline);
-	if (this->GPIOflag) free ((int*)this->GPIOflag);
-	pthread_mutex_destroy(&this->mutex_sensor);
-	free (this);
-}
-
-
-int
-sensorIR_get_GPIOline (sensor_t* this)
-{
-	//pthread_mutex_lock(&this->mutex_sensor);
-	return this->GPIOline;
-	//pthread_mutex_unlock(&this->mutex_sensor);
-}
-
-void
-sensorIR_set_GPIOline (sensor_t* this, int newGPIOline)
-{
-	//pthread_mutex_lock(&this->mutex_sensor);
-	this->GPIOline = newGPIOline;
-	//pthread_mutex_unlock(&this->mutex_sensor);
+	if (this->id)
+		free((int*) this->id);
+	if (this->GPIOlines)
+		free((int*) this->GPIOlines);
+	pthread_mutex_destroy(&this->mutex_sensorIR);
+	free(this);
 }
 
 int
-sensorIR_get_GPIOflag (sensor_t* this)
+sensorIR_get_id(sensorIR_t* this)
 {
-	//pthread_mutex_lock(&this->mutex_sensor);
-	return this->GPIOflag;
-	//pthread_mutex_unlock(&this->mutex_sensor);
+	return this->id;
 }
 
 void
-sensorIR_set_GPIOflag (sensor_t* this, int newGPIOflag)
+sensorIR_set_id(sensorIR_t* this, int newid)
 {
-	//pthread_mutex_lock(&this->mutex_sensor);
-	this->GPIOflag = newGPIOflag;
-	//pthread_mutex_unlock(&this->mutex_sensor);
-}
-
-void
-sensorIR_rise_GPIOflag (sensor_t* this)
-{
-	//pthread_mutex_lock(&this->mutex_sensor);
-	this->GPIOflag = HIGH;
-	//pthread_mutex_unlock(&this->mutex_sensor);
-}
-
-void
-sensorIR_clear_GPIOflag (sensor_t* this)
-{
-	//pthread_mutex_lock(&this->mutex_sensor);
-	this->GPIOflag = LOW;
-	//pthread_mutex_unlock(&this->mutex_sensor);
-}
-
-void
-sensorIR_storeSensor(sensor_t* this)
-{
-	//pthread_mutex_lock(&this->mutex_sensor);
-	this->GPIOflag = digitalRead(this->GPIOline);
-	//pthread_mutex_unlock(&this->mutex_sensor);
+	this->id = newid;
 }
 
 int
-sensorIR_readSensor(sensor_t* this)
+sensorIR_readLine(sensorIR_t* this, int trainLine) //train: 0 for diesel, 1 for renfe
 {
-	//pthread_mutex_lock(&this->mutex_sensor);
-	sensorIR_storeSensor(this);
-	return this->GPIOflag;
-	//pthread_mutex_unlock(&this->mutex_sensor);
+	int r;
+	r = digitalRead(this->GPIOlines[trainLine]);
+	return r;
 }
 
+int
+sensorIR_trainPassing(sensorIR_t* this)
+{
+	int i, r;
+	r=-1;
+
+	for (i = 0; i < NUMBER_OF_TRAINS; i++) {
+		if (digitalRead(this->GPIOlines[i]) == HIGH) {
+			r=i;
+		}
+	}
+
+	return r; //returns 0 if diesel is passing, 1 if renfe is passing, or -1 if no one
+}
 
---
+++ SoftwarePi/Model/Sensors/sensorIR.h
@@ -0,0 +1,36 @@
+/*
+ * sensorIR.h
+ *
+ *  Created on: 02/04/2014
+ *      Author: Javier
+ */
+
+#ifndef SENSORIR_H
+#define SENSORIR_H
+
+#include <pthread.h>
+
+#define NUMBER_OF_TRAINS 2 //esto habría que sacarlo de aquí cuanto antes
+
+struct sensorIR_t;
+
+typedef struct sensorIR_t {
+
+	int id;
+	int GPIOlines[NUMBER_OF_TRAINS];
+
+	pthread_mutex_t mutex_sensorIR;
+
+} sensorIR_t;
+
+sensorIR_t* sensorIR_new (int id, int prio, int ceil);
+void sensorIR_init (sensorIR_t* this, int id, int prio, int ceil);
+void sensorIR_destroy (sensorIR_t* this);
+
+int sensorIR_get_id (sensorIR_t* this);
+void sensorIR_set_id (sensorIR_t* this, int newid);
+
+int sensorIR_readLine(sensorIR_t* this, int trainLine);
+int sensorIR_trainPassing(sensorIR_t* this);
+
+#endif
