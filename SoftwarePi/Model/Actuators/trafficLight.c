7f640e44bdc80e5312e39417fc1b704564ffd1c8
Jose Martin <jose.martin.burgos@gmail.com>
2014-04-08 18:49:38 +0200

    Fixed trafficLight, mutexes, trainlist

--- SoftwarePi/Model/Actuators/trafficLight.c
+++ SoftwarePi/Model/Actuators/trafficLight.c
@@ -1,24 +1,42 @@
+#include <stdbool.h>
 #include "trafficLight.h"
 
-void
-trafficLight_init (trafficLight_t* this, state_t state)
-{
-	this->state = state;
-	pthread_mutex_init (&this->mutex, NULL);
+#include <native/mutex.h>
 
+
+void
+trafficLight_init (trafficLight_t* this, int id, state_t state)
+{
+	actuator_init ((actuator_t*) this, id, traffic_light_notify);
+	
+	this->state = state;
+	this->activeTrainsCount = 0;
+	this->sectors.sectorCount = 0;
+	rt_mutex_create (&this->mutex, NULL);
+	
 }
 
 trafficLight_t*
-trafficLight_new (state_t state)
+trafficLight_new (int id)
 {
 	trafficLight_t* this = (trafficLight_t*) malloc (sizeof (trafficLight_t));
-	trafficLight_init (this, state);
+	trafficLight_init (this, id, OFF);
 
 	return this;
 
 }
 
+void 
+trafficLight_set_sensibleSector (trafficLight_t* this, int sector, bool direction)
+{
+	if (this->sectors.sectorCount == MAXSENSIBLESECTORS)
+		return;
+	
+	this->sectors.sectors[this->sectors.sectorCount] = sector;
+	this->sectors.sectors[this->sectors.sectorCount] = direction;
+	this->sectors.sectorCount++;
 
+}
 state_t
 trafficLight_get_state (trafficLight_t* this)
 {
@@ -31,8 +49,19 @@ return this->state;
 void
 trafficLight_set (trafficLight_t* this, state_t state)
 {
-pthread_mutex_lock (&this->mutex);
-this->state = state;
-pthread_mutex_unlock (&this->mutex);
+	rt_mutex_acquire(&(this->mutex), TM_INFINITE);
+	this->state = state;
+	pthread_mutex_unlock (&this->mutex);
+	rt_mutex_release(&(this->mutex));
 
 }
+
+void
+traffic_light_notify (observer_t* this, observable_t* observable)
+{
+    /*
+     * TRAFFIC LIGHTS BEHAVIOUR GOES HERE
+     */
+}
+
+
--- SoftwarePi/Model/Actuators/trafficLight.h
+++ SoftwarePi/Model/Actuators/trafficLight.h
@@ -3,26 +3,61 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+#include <native/mutex.h>
+#include <actuator.h>
+
+#define MAXTRAINS 10
+
+#define MAXSENSIBLESECTORS 2
+
+typedef struct sensibleSectors_t
+{
+	int sectorCount;
+	
+	int sectors[MAXSENSIBLESECTORS];
+	
+	bool direction [MAXSENSIBLESECTORS];
+} sensibleSectors_t;
+
+
 typedef enum {ON, OFF} state_t;
 
 typedef struct trafficLight_t
 {
+	//Inheritance
+    actuator_t actuator;
+    
 	state_t state;
+	
+	sensibleSectors_t sectors;
+	
+	int activeTrainsCount;
+	int activeTrains[MAXTRAINS];
 
-	pthread_mutex_t mutex;
+	RT_MUTEX mutex;
 
 } trafficLight_t;
 
 
-//----------------------------
+/*
+ * Object creation / destruction
+ */
+void traffic_light_init (traffic_light_t* this, int id, state_t state);
 
-void trafficLight_init (trafficLight_t* this, state_t state);
+trafficLight_t* trafficLight_new (int id);
 
-trafficLight_t* trafficLight_new (state_t state);
-
-
+/*
+ * Getters / setters
+ */
 state_t trafficLight_get_state (trafficLight_t* this);
+void trafficLight_set_sensibleSector (trafficLight_t* this, int sector, bool sentido);
+void trafficLight_set_state (trafficLight_t* this, state_t state);
 
-void trafficLight_set (trafficLight_t* this, state_t state);
+/*
+ * Observer function
+ *
+ * Traffic lights behaviour belongs here
+ */
+void traffic_light_notify (observer_t* this, observable_t* observable);
 
 #endif
