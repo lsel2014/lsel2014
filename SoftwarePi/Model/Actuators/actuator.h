83687e02d91b7a82e1acf06e78c3c18d9a60990c
Samuel Lopez <user@lsel-2014.(none)>
2014-04-08 17:30:32 +0200

    Fixed actuators

--- SoftwarePi/Model/Actuators/actuator.h
+++ SoftwarePi/Model/Actuators/actuator.h
@@ -1,46 +1,25 @@
 #ifndef ACTUATOR_H
 #define ACTUATOR_H
-#include "rail_change.h"
-#include "traffic_light.h"
-#include "sun.h"
-#include "frames_gen.h"
-#include <stdlib.h>
+
+#include "../observer.h"
+
+typedef void (*actuator_notify_func_t) (observer_t*, observable_t*);
 
 typedef struct actuator_t
-{	
+{
+    //Inheritance
+    observer_t observer;
 
-	rail_change_t* railChange;
-	traffic_light_t* trafficLight;
-	sun_t* sun;
-	frames_t* frameGen;
-
-	pthread_mutex_t mutex;
+    //Attributes
+    int id;
 
 } actuator_t;
 
 
 /*
- * Actuators init and creation
+ * Virtual class, no constructor
  */
+void actuator_init (actuator_t* this, int id, actuator_notify_func_t notify);
 
-actuator_t* actuator_new (sun_date_t date, direction_t initialRailway);
-
-/*
- * Setters
- */
-
-void actuator_set_rail_change (actuator_t* this, rail_change_t* rail_change);
-void actuator_set_traffic_light (actuator_t* this, traffic_light_t* traffic_light);
-void actuator_set_sun (actuator_t* this, sun_t* sun);
-void actuator_set_frame_gen (actuator_t* this, frames_t* frame_gen);
-
-/*
- * Getters
- */ 
-
-rail_change_t* actuator_get_rail_change (actuator_t* this);
-traffic_light_t* actuator_get_traffic_light (actuator_t* this);
-sun_t* actuator_get_sun (actuator_t* this);
-frames_t* actuator_get_frame_gen (actuator_t* this);
 
 #endif
