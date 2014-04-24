977d332f9193d21d589a1e926cb0377df629058f
tomasvalno <tomasvalno@gmail.com>
2014-04-24 19:54:54 +0200

    Update model.c

--- SoftwarePi/model.c
+++ SoftwarePi/model.c
@@ -38,7 +38,7 @@ void IRsensors_poll(void* arg) {
 		int i = 0;
 		rt_task_wait_period(NULL);
 		for (i = 0; i < n_ir_sensors; i++) {
-			printf("U JELLY?");
+			printf("U JELLY?%c",ir_sensorsmodel[i].name);
 			sensorIR_trainPassing(ir_sensorsmodel[i].sensor);
 		}
 	}
