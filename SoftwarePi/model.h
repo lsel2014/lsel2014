b648644b5b2cc6933fab7c72f7f0b15a9b9fc31c
tomasvalno <tomasvalno@gmail.com>
2014-04-25 14:12:33 +0200

    Update model.h

--- SoftwarePi/model.h
+++ SoftwarePi/model.h
@@ -4,7 +4,7 @@
 #include "Model/observer.h"
 
 void model_init (void);
-observable_t* model_get_obs (const char* name);
+observable_t* model_get_observable (const char* name);
 int model_add (const char* name, observable_t* obs);
 
 #endif
