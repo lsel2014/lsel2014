c52521b98ddd40e241d7ec43607339671a414567
tomasvalno <tomasvalno@gmail.com>
2014-04-26 18:50:29 +0200

    Update tracker.c

--- SoftwarePi/tracker.c
+++ SoftwarePi/tracker.c
@@ -6,8 +6,7 @@
 
 
 static observer_t tracker_observer;
-// Arrays donde se guardan los elementos de la maqueta
-// que interesan al tracker. 
+// Arrays where model elements are stored
 static struct ir_sensor_data_t {
 		sensorIR_t* sensor;
 		char sector;
@@ -18,12 +17,15 @@ static struct train_data_t {
 		int IRsimbolicId;
 } tracker_trains [MAXTRAINS];
 
+static struct railway_data_t {
+		railway_t* train;
+		int platform;
+} tracker_railway [MAXRAILWAY];
 static int n_ir_sensors;
 static int n_trains;
-
+static int n_railway;
 static event_t* event;
-// Esta funcion solo traduce la id simbolica al 
-// puntero de tren, la he sacado para limpiar un poco
+// Translate the ID ir sensors returns into the actual train pointer
 train_t*
 tracker_gen_train(int id){
      struct train_data_t* t;             
@@ -44,9 +46,8 @@ timeval_sub (struct timeval *res, struct timeval *a, struct timeval *b)
   }
 }
                    
-// El notify revisa los sensores que estan registrados 
-// y si ve que alguno tiene algun evento, lo lee y 
-// actualiza la maqueta 
+// Notify checks registered sensors and if some of them
+// has an event and it's not a rebound updates the model.
 static
 void tracker_notify (observer_t* this, observable_t* foo)
 {
@@ -65,7 +66,6 @@ void tracker_notify (observer_t* this, observable_t* foo)
 	    	last = tel-> timestamp;
 	    	gettimeofday (&now, NULL);
 	    	timeval_sub ( &diff , &now , &last );
-	    	// divido entre microsegundos para tener mayor precision
 	    	speed = LENGHTSECTOR / diff -> tv_usec ;
 	    	train_set_current_speed(train , speed );
 	    	//railway_register_train(tracker_gen_train(event->passingTrain), p->sector);
@@ -76,8 +76,7 @@ void tracker_notify (observer_t* this, observable_t* foo)
 void
 tracker_init (void)
 {
-    // Estructura donde metemos los nombres que los sensores van a tener
-    // en el modelo y sus sectores asociados. 
+    // Struct with the names and associated sector of the sensors
 	static struct ir_name_t {
 			const char* name;
 			char sector;
@@ -88,10 +87,7 @@ tracker_init (void)
 			{ "IRsensor[3]", 3 },
 			{ NULL, 0 }
 	};
-	
-	struct ir_name_t* s;
-	// Estructura donde metemos los nombres que los trenes van a tener
-    // en el modelo y las id que tienen en los sensores IR. 
+	// Struct with the names and the ID of each train in the IR sensors
 	static struct train_name_t {
 			const char* name;
 			int IRsimbolicId;
@@ -100,15 +96,24 @@ tracker_init (void)
 			{ "Renfe", 3 },
 			{ NULL, 0 }
 	};
-	
+	static struct railway_name_t {
+			const char* name;
+			int platform;
+	} railway_names [] = {
+			{ "via[0]", 0 },
+			{ NULL, 0 }
+	};
+	struct ir_name_t* s;
 	struct train_name_t* t;
-	// Inicializamos el observer del tracker
+	struct railway_name_t* r;
 	observer_init (&tracker_observer, tracker_notify);
 	
 	n_ir_sensors = 0;
 	n_trains=0;
-	// Estos bucles se dedican a coger los sensores y los trenes
-	// del modelo y registrarlos en el tracker 
+	n_railway=0;
+	// those "for" takes the elements in the model, cast them to 
+	// his type ( since all the elements in the model are observables)
+	// and store them in the appropriate struct.
 	for (s = ir_names; s->name; ++s) {
 		observable_t* obs = model_get_observable (s->name);
 		observable_register_observer (obs, &tracker_observer);
@@ -120,12 +125,18 @@ tracker_init (void)
 	for (t = train_names; t->name; ++t) {
 		observable_t* obs = model_get_observable (t->name);
 		observable_register_observer (obs, &tracker_observer);
-		tracker_trains[n_ir_sensors].train = (train_t*) obs;
-		tracker_trains[n_ir_sensors].IRsimbolicId = t->IRsimbolicId;
+		tracker_trains[n_trains].train = (train_t*) obs;
+		tracker_trains[n_trains].IRsimbolicId = t->IRsimbolicId;
 		++n_trains;
 	}
 	
-	// Comentario: cuando este la via, hay que cogerla tambien.
+	for( r = railway_names; t->name; ++r) {
+		observable_t* obs = model_get_observable (r->name);
+		observable_register_observer (obs, &tracker_observer);
+		tracker_railway[n_railway].train = (railway_t*) obs;
+		tracker_railway[n_railway].platform = r->platform;
+		++n_railway;
+	}
 }
 
 
