274c3795c921167ba3c7bc9b912adffea1eeb5d5
tomasvalno <tomasvalno@gmail.com>
2014-04-28 16:00:27 +0200

    Update tracker.c

--- SoftwarePi/tracker.c
+++ SoftwarePi/tracker.c
@@ -27,7 +27,8 @@ static int n_trains;
 static int n_railway;
 static event_t* event;
 
-void timeval_sub(struct timeval *res, struct timeval *a, struct timeval *b) {
+void
+timeval_sub(struct timeval *res, struct timeval *a, struct timeval *b) {
 	res->tv_sec = a->tv_sec - b->tv_sec;
 	res->tv_usec = a->tv_usec - b->tv_usec;
 	if (res->tv_usec < 0) {
