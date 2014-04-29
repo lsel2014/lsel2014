b62e12a397147711ede322a6ed0adf7b6c7bba6c
tomasvalno <tomasvalno@gmail.com>
2014-04-29 19:56:15 +0200

    Update train.h

--- SoftwarePi/train.h
+++ SoftwarePi/train.h
@@ -69,7 +69,7 @@ char train_get_n_wagon(train_t* this);
 char train_get_length(train_t* this);
 telemetry_t* train_get_telemetry(train_t* this);
 char train_get_sector (train_t* this);
-void train_get_timestamp (train_t* this, struct timeval *tv)
+void train_get_timestamp (train_t* this, struct timeval *tv);
 float train_get_speed(train_t* this);
 char train_get_security(train_t* this);
 void train_set_security(train_t* this, char newSecurity);
