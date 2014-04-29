91c91e2b9fc77853b27182ad5118c5f374ad29d1
tomasvalno <tomasvalno@gmail.com>
2014-04-29 16:50:06 +0200

    Update tracker.h

--- SoftwarePi/tracker.h
+++ SoftwarePi/tracker.h
@@ -6,8 +6,8 @@
 void tracker_init(void);
 void timeval_sub(struct timeval *res, struct timeval *a, struct timeval *b);
 train_t* tracker_gen_train(int id);
-void tracker_updating_train(train_t* train, int sector, telemetry_t* tel);
-void tracker_register_train(train_t* train, int sector);
+void tracker_updating_train(train_t* train, char sector, telemetry_t* tel);
+//void tracker_register_train(train_t* train, char sector);
 static void tracker_notify(observer_t* this, observable_t* foo);
 void tracker_init(void);
 
