c19d323849b133fba10fb08728d82f0df0e5ea18
tomasvalno <tomasvalno@gmail.com>
2014-05-19 17:15:06 +0200

    Update daemon.c

--- daemon.c
+++ daemon.c
@@ -1,143 +1,255 @@
-#include <stdio.h>
-#include <signal.h>
+#include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
-// Xenomai native skin libraries
-#include <sys/mman.h>
-#include <native/task.h>
-#include <native/timer.h>
-// WiringPi
-#include <wiringPi.h>
+#define MAXCMDS 100
 
-// RT compilant print library
-#include <rtdk.h>
+static int done = 0;
 
-// Tasks
-#include "dcc.h"
+int com_help (char* arg);
+int com_sleep (char* arg);
+int com_quit (char* arg);
 
-// Interpreter
-#include "interp.h"
-#include "daemon.h"
-#include "tasks.h"
+typedef struct {
+  char *name;
+  int (*func) (char*);
+  char *doc;
+} cmd_t;
 
-//Model
-#include "train.h"
-#include "sun.h"
-#include "model.h"
-#include "sensorIR.h"
-#include "railChange.h"
-#include "trafficLight.h"
-#include "semaphore.h"
-#include "crossingGate.h"
-#include "tracker.h"
-#include "railway.h"
-#include "anticollision.h"
-#include "screen.h"
-#include "ctrlIlumination.h"
+cmd_t commands[MAXCMDS] = {
+  { "help", com_help, "Display this text" },
+  { "?", com_help, "Synonym for `help'" },
+  { "sleep", com_sleep, "Wait <n> seconds" },
+  { "quit", com_quit, "Quit" },
+  { (char *) NULL, NULL, (char *) NULL },
+};
 
-// Dummy function to catch signals
-void catch_signal() {
+char* stripwhite (char *string);
+cmd_t* find_command (char *name);
+void initialize_readline (void);
+int execute_line (char *line);
+
+int
+interp_addcmd (char* cmd, int (*cmd_func) (char*), char* doc)
+{
+  int i;
+  for (i = 0; i < (MAXCMDS - 1); ++i) {
+    if (! commands[i].name) {
+      commands[i].name = cmd;
+      commands[i].func = cmd_func;
+      commands[i].doc = doc;
+      commands[++i].name = NULL;
+      return 1;
+    }
+  }
+  return 0;
 }
 
-void initializeModel(void) {
-	// TODO
-	int i;
+void
+interp_run (void)
+{
+  initialize_readline ();
+  while (! done) {
+    char* s;
+    char* line;
 
-	for (i = 0; i < 4; i++) {
-		//IRsensors[i] = sensorIR_new(i);
-	}
+    line = readline (">>> ");
+    if (!line)
+      break;
+
+    s = stripwhite (line);
+    if (*s) {
+      add_history (s);
+      execute_line (s);
+    }
+
+    free (line);
+  }
 }
 
-void initializeXenomaiEnv(void) {
-	// Catch signals
-	signal(SIGTERM, catch_signal);
-	signal(SIGINT, catch_signal);
 
-	/* Avoids memory swapping for this program */
-	mlockall(MCL_CURRENT | MCL_FUTURE);
+int
+execute_line (char *line)
+{
+  register int i;
+  cmd_t *command;
+  char *word;
 
-	// Initialize rdtk to use rt_printf
-	rt_print_auto_init(1);
+  /* Isolate the command word. */
+  i = 0;
+  while (line[i] && isspace (line[i]))
+    i++;
+  word = line + i;
+
+  while (line[i] && !isspace (line[i]))
+    i++;
+
+  if (line[i])
+    line[i++] = '\0';
+
+  command = find_command (word);
+
+  if (!command)
+    {
+      fprintf (stderr, "%s: No such command.\n", word);
+      return (-1);
+    }
+
+  /* Get argument to command, if any. */
+  while (isspace (line[i]))
+    i++;
+
+  word = line + i;
+
+  /* Call the function. */
+  return (*(command->func)) (word);
 }
 
-void initializeWiringPi(void) {
-
-	wiringPiSetup();
-
-	// Replace with proper I2C modules load on kernel
-	//system("gpio load i2c");
-
-	//pinModes ....
+/* Look up NAME as the name of a command, and return a pointer to that
+command. Return a NULL pointer if NAME isn't a command name. */
+cmd_t *
+find_command (char *name)
+{
+  int i;
+  for (i = 0; commands[i].name; i++)
+    if (strcmp (name, commands[i].name) == 0)
+      return (&commands[i]);
+  return (cmd_t *) NULL;
 }
 
-int main(int argc, char* argv[]) {
+char *
+stripwhite (char *string)
+{
+  register char *s, *t;
 
-	rt_printf("xeno");
-	// Initialize Xenomai RT enviroment
-	initializeXenomaiEnv();
-	rt_printf("wiringpi");
+  for (s = string; isspace (*s); s++)
+    ;
+    
+  if (*s == 0)
+    return (s);
 
-	// Initialize wiringPi lib, configure IO
-	initializeWiringPi();
+  t = s + strlen (s) - 1;
+  while (t > s && isspace (*t))
+    t--;
+  *++t = '\0';
 
-	// Initialize the model
-	//initializeModel();
+  return s;
+}
 
-	// Initialize the train controller
-	//trainCtrl_init();
 
-	/*
-	 * Arguments: &task,
-	 * name,
-	 * stack size (0=default),
-	 * priority,
-	 * mode (FPU, start suspended, ...)
-	 */
-	//rt_task_create(&task_dcc, "dccSend", 0, TASK_DCC_PRIORITY, 0);
-	//rt_task_create(&task_poll, "polling", 0, TASK_POLL_PRIORITY, 0);
-	//rt_task_create(&task_sun, "sun", 0, TASK_SUN_PRIORITY, 0);
-	/*
-	 * Arguments: &task,
-	 * task function,
-	 * function argument*/
-	IRsensors_setup();
-	rt_printf("IR");
-	trains_setup();
-	rt_printf("trains");
-	railways_setup();
-	rt_printf("railway");
-	semaphore_setup();
-	rt_printf("semaphore");
-	trafficLight_setup();
-	rt_printf("traffic Light");
-	crossingGate_setup();
-	rt_printf("Crossing Gate");
-	screen_setup();
-	rt_printf("screen");
-	setupRailChange();
-	rt_printf("rail change");
-	sun_setup();
-	rt_printf("sun");
-	model_init();
-	rt_printf("model");
-	anticollision_setup();
-	rt_printf("anti");
-	tracker_init();
-	rt_printf("tracker");
-	ctrlilumination_init();
-	rt_printf("crtl ilu");
-	// TODO Hay que darle argumentos a la tarea!
-	//rt_task_start(&task_dcc, &dcc_send, NULL );
-	//rt_task_start(&task_poll, &daemon_poll_sensors, IRsensors);
-	// rt_task_start(&task_sun, &daemon_update_sun, NULL );
-	task_start_all();
-	interp_run();
+/* Interface to Readline Completion */
 
-	// Remove the permanent tasks
-	//rt_task_delete(&task_poll);
-	//rt_task_delete(&task_dcc);
-	//rt_task_delete(&task_sun);
-	task_delete_all();
-	screen_destroy();
-	return 0;
+char *command_generator ();
+char **interp_completion ();
+
+void
+initialize_readline (void)
+{
+  /* Allow conditional parsing of the ~/.inputrc file. */
+  rl_readline_name = "ISEL2014";
+
+  /* Tell the completer that we want a crack first. */
+  rl_attempted_completion_function = (CPPFunction *)interp_completion;
+}
+
+/* Attempt to complete on the contents of TEXT. START and END bound the
+region of rl_line_buffer that contains the word to complete. TEXT is
+the word to complete. We can use the entire contents of rl_line_buffer
+in case we want to do some simple parsing. Return the array of matches,
+or NULL if there aren't any. */
+char **
+interp_completion (char *text, int start, int end)
+{
+  char **matches;
+
+  matches = (char **)NULL;
+
+  /* If this word is at the start of the line, then it is a command
+to complete. Otherwise it is the name of a file in the current
+directory. */
+  if (start == 0)
+    matches = completion_matches (text, command_generator);
+
+  return (matches);
+}
+
+/* Generator function for command completion. STATE lets us know whether
+to start from scratch; without any state (i.e. STATE == 0), then we
+start at the top of the list. */
+char *
+command_generator (char *text, int state)
+{
+  static int list_index, len;
+  char *name;
+
+  /* If this is a new word to complete, initialize now. This includes
+saving the length of TEXT for efficiency, and initializing the index
+variable to 0. */
+  if (!state) {
+    list_index = 0;
+    len = strlen (text);
+  }
+
+  /* Return the next name which partially matches from the command list. */
+  while ((name = commands[list_index].name)) {
+    list_index++;
+
+    if (strncmp (name, text, len) == 0)
+      return strdup (name);
+  }
+
+  /* If no names matched, then return NULL. */
+  return (char *) NULL;
+}
+
+int
+com_help (char *arg)
+{
+  int i;
+  int printed = 0;
+
+  for (i = 0; commands[i].name; i++) {
+    if (!*arg || (strcmp (arg, commands[i].name) == 0)) {
+      printf ("%s\t\t%s.\n", commands[i].name, commands[i].doc);
+      printed++;
+    }
+  }
+
+  if (!printed) {
+    printf ("No commands match `%s'. Possibilties are:\n", arg);
+
+    for (i = 0; commands[i].name; i++) {
+      /* Print in six columns. */
+      if (printed == 6) {
+        printed = 0;
+        printf ("\n");
+      }
+
+      printf ("%s\t", commands[i].name);
+      printed++;
+    }
+
+    if (printed)
+      printf ("\n");
+  }
+  return 0;
+}
+
+
+int
+com_sleep (char *arg)
+{
+  sleep (atoi(arg));
+  return 0;
+}
+
+
+int
+com_quit (char *arg)
+{
+  done = 1;
+  return 0;
 }
