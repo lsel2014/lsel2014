8325271a67b4e844140fbb78b3985939bb037650
tomasvalno <tomasvalno@gmail.com>
2014-05-26 18:06:46 +0200

    Update semaphore.c

--- semaphore.c
+++ semaphore.c
@@ -6,7 +6,7 @@ semaphore_t* semaphores[MAXSEMAFORES];
 int nsemaphores;
 
 semaphore_t* 
-semaphore_new(int id, state_t state)
+semaphore_new(int id, int state)
 {
     semaphore_t* this = (semaphore_t*) malloc(sizeof(semaphore_t));
 	semaphore_init(this, id , state);
@@ -18,7 +18,7 @@ semaphore_new(int id, state_t state)
 	return this;
 }
 void 
-semaphore_init(semaphore_t* this, int id, state_t state)
+semaphore_init(semaphore_t* this, int id, int state)
 {
 	observable_init(&this->observable);
 	this->id = id;
@@ -30,7 +30,7 @@ semaphore_setup(void)
 {
     int i;
     for( i=0; i<MAXSEMAFORES; i++){
-         semaphore_new(i,OFF);
+         semaphore_new(i,0);
          }
 }
 void 
