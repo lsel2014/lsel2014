ead218141e1de68fd83034caffec2892e8bf2011
tomasvalno <tomasvalno@gmail.com>
2014-05-26 18:01:35 +0200

    Update semaphore.c

--- semaphore.c
+++ semaphore.c
@@ -6,10 +6,10 @@ semaphore_t* semaphores[MAXSEMAFORES];
 int nsemaphores;
 
 semaphore_t* 
-semaphore_new(int id)
+semaphore_new(int id, state_t state)
 {
     semaphore_t* this = (semaphore_t*) malloc(sizeof(semaphore_t));
-	semaphore_init(this, id);
+	semaphore_init(this, id , state);
 	
     if (nsemaphores < MAXSEMAFORES) {
 		semaphores[nsemaphores++] = this;
@@ -18,11 +18,11 @@ semaphore_new(int id)
 	return this;
 }
 void 
-semaphore_init(semaphore_t* this, int id)
+semaphore_init(semaphore_t* this, int id, state_t state)
 {
 	observable_init(&this->observable);
 	this->id = id;
-
+	this->state = state;
 	rt_mutex_create(&this->mutex, NULL);
 }
 void 
@@ -30,7 +30,7 @@ semaphore_setup(void)
 {
     int i;
     for( i=0; i<MAXSEMAFORES; i++){
-         semaphore_new(i);
+         semaphore_new(i,OFF);
          }
 }
 void 
@@ -44,6 +44,6 @@ void
 semaphore_switch_off(int i)
 {
     semaphore_t* local = semaphores[i];
-    local->state = ON;
+    local->state = OFF;
     rt_printf(" semafore %d switched to %s \n", i , semaphores[i]->state);                        
 }
