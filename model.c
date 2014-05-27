3f5b302b49bae9383a7ea83ab5c5549970f3a92e
tomasvalno <tomasvalno@gmail.com>
2014-05-27 16:31:15 +0200

    Update model.c

--- model.c
+++ model.c
@@ -1,26 +1,27 @@
-#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "sensorIR.h"
 #include "train.h"
 #include "model.h"
-#include "interp.h"
 #include "railway.h"
 #include "crossingGate.h"
 #include "semaphore.h"
 #include "trafficLight.h"
 
-#define MAXOBSERVABLES 30
+#define MAX_OBJS 100
 
-static struct registered_observable_t {
+static struct object_ref_t {
 	observable_t* obs;
 	const char* name;
-} observablesmodel[MAXOBSERVABLES];
+} objs[MAX_OBJS];
 
-static int n_observable;
+static int n_objs = 0;
 
 int model_add_observable(const char* name, observable_t* obs) {
 	if (n_observable <= MAXOBSERVABLES) {
-		observablesmodel[n_observable].name = name;
-		observablesmodel[n_observable].obs = obs;
+		objs[n_objs].name = name;
+		objs[n_objs].obs = obs;
+		n_objs++;
 		return 1;
 	}
 	printf("Max. sensor registered\n");
@@ -65,38 +66,32 @@ void model_init(void) {
 	
 	for (s = ir_names; s->name; ++s) {
 		model_add_observable(s->name, (observable_t*) sensors[i]);
-		++n_observable;
 		i++;
 	}
 	
 	i = 0;
 	for (t = train_names; t->name; ++t) {
 		model_add_observable(t->name, (observable_t*) trains[i]);
-		++n_observable;
 		i++;
 	}
 	i = 0;
 		for (r = railway_names; r->name; ++r) {
 		model_add_observable(r->name, (observable_t*) railways[i]);
-		++n_observable;
 		i++;
 	}
 	i = 0;
 	for (cr = crossingGate_names; cr->name; ++cr) {
 		model_add_observable(cr->name, (observable_t*) crossingGates[i]);
-		++n_observable;
 		i++;
 	}
 	i = 0;
 	for (tl = trafficlight_names; tl->name; ++tl) {
 		model_add_observable(tl->name, (observable_t*) trafficLights[i]);
-		++n_observable;
 		i++;
 	}
 	i = 0;
 	for (sph = semaphore_names; sph->name; ++sph) {
 		model_add_observable(sph->name, (observable_t*) semaphores[i]);
-		++n_observable;
 		i++;
 	}
 	i = 0;
