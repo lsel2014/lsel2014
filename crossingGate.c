b31af7ab92d0b012e0220ed30e440058a5b2cb3c
tomasvalno <tomasvalno@gmail.com>
2014-05-27 21:52:55 +0200

    Update crossingGate.c

--- crossingGate.c
+++ crossingGate.c
@@ -7,6 +7,11 @@
 #include "tasks.h"
 #include "crossingGate.h"
 #include "interp.h"
+// Random values, this will change to match the firmware of the barrier
+#define I2C_BARRIER_ADRESS 0x21
+#define I2C_BARRIER_UP 0x00
+#define I2C_BARRIER_DOWN 0x01
+#define I2C_BARRIER_STOP 0x02
 
 crossingGate_t* crossingGates[MAXCROSINGGATES];
 int n_crossingGates;
@@ -15,7 +20,7 @@ crossingGate_setup(void)
 {
     int sensec[] = {2,3};
     n_crossingGates=0;
-	crossingGates[0] = crossingGate_new(0, 11,sensec);
+	crossingGates[0] = crossingGate_new(0, 11,sensec,I2C_BARRIER_ADRESS);
 	
 	interp_addcmd("barrier", crossingGate_cmd, "Set barrier state\n");
 	
@@ -37,7 +42,7 @@ crossingGate_cmd(char* arg)
 	}
 	return 1;
 }
-
+/*
 crossingGate_t*
 crossingGate_new(int id, int GPIOline,int sensiblesectors[2]) {
 	crossingGate_t* this = (crossingGate_t*) malloc(sizeof(crossingGate_t));
@@ -66,7 +71,36 @@ crossingGate_init(crossingGate_t* this, int id, int GPIOline,
 	crossingGate_set_position(this, position);
 
 }
+*/
+crossingGate_t*
+crossingGate_new(int id, int GPIOline,int sensiblesectors[2], uint16_t i2c_address) {
+	crossingGate_t* this = (crossingGate_t*) malloc(sizeof(crossingGate_t));
+	crossingGate_init(this, id, GPIOline, DOWN, sensiblesectors,i2c_address);
+    if (n_crossingGates < MAXCROSINGGATES) {
+		crossingGates[n_crossingGates++] = this;
+	}
+	return this;
+}
 
+void 
+crossingGate_init(crossingGate_t* this, int id, int GPIOline,
+		                          position_t position, int sensiblesectors[2], uint16_t i2c_address) {
+	observable_init(&this->observable);
+	this->GPIOline = GPIOline;
+	this->position = position;
+	this->id = id;
+	this->needsService = 0;
+	this->i2c_address=i2c_address;
+        this->sensiblesectors[0] = sensiblesectors[0];
+        this->sensiblesectors[1] = sensiblesectors[1];
+	pinMode(GPIOline, OUTPUT);
+	//softPwmCreate(GPIOline, 0, 200);
+
+	rt_mutex_create(&this->mutex, NULL);
+	task_add("Gate control", GATE_DEADLINE, crossingGate_move_task, this);
+	crossingGate_set_position(this, position);
+
+}
 void 
 crossingGate_destroy(crossingGate_t* this) 
 {
@@ -90,7 +124,7 @@ crossingGate_set_position(crossingGate_t* this, position_t position)
 	rt_mutex_release(&this->mutex);
 	//rt_printf("gate switched");
 }
-
+/*
 void 
 crossingGate_move_task(void *args) 
 {
@@ -110,6 +144,33 @@ crossingGate_move_task(void *args)
 			rt_mutex_release(&this->mutex);
 		} else {
 			softPwmWrite(this->GPIOline, 0);
+		}
+	}
+}
+*/
+crossingGate_move_task(void *args) 
+{
+	crossingGate_t* this = (crossingGate_t*) args;
+	uint16_t barrier_comand[2] ;
+	uint8_t status;
+	rt_task_set_periodic(NULL, TM_NOW, GATE_PERIOD);
+	while (1) {
+		rt_task_wait_period(NULL);
+		uint16_t barrier_comand[0]=(I2C_BARRIER_ADRESS<<1);
+		if (this->needsService) {
+			rt_mutex_acquire(&(this->mutex), TM_INFINITE);
+
+			if (this->position == DOWN) {
+				uint16_t barrier_comand[0]=I2C_BARRIER_DOWN;
+			} else {
+				uint16_t barrier_comand[0]=I2C_BARRIER_UP;
+			}
+			i2c_send_sequence(i2c0handle, barrier_comand, 2, &status);
+			this->needsService = 0;
+			rt_mutex_release(&this->mutex);
+		} else {
+			uint16_t barrier_comand[0]=I2C_BARRIER_STOP;
+			i2c_send_sequence(i2c0handle, barrier_comand, 2, &status);
 		}
 	}
 }
