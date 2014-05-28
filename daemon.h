6f8e997703a198e78a830641ae3977c509d28bd4
tomasvalno <tomasvalno@gmail.com>
2014-05-28 19:20:18 +0200

    Update daemon.h

--- daemon.h
+++ daemon.h
@@ -2,10 +2,18 @@
 #define DAEMON_H
 
 // Task priorities
+#include <native/mutex.h>
+
 #define TASK_DCC_PRIORITY 40
 #define TASK_POLL_PRIORITY 30
 #define TASK_SUN_PRIORITY 20
 #define TASK_INTERPRETER_PRIORITY 10
 
-extern int i2c0handle,i2c1handle;
+typedef struct{
+        int i2chandler;
+        RT_MUTEX mutex;
+}i2chandler_t;
+
+extern i2chandler_t* i2c0handle,i2c1handle;
+
 #endif
