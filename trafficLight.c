9ff394d9b6bde68e326647df75bc8704cac21d6f
tomasvalno <tomasvalno@gmail.com>
2014-05-28 21:29:26 +0200

    Update trafficLight.c

--- trafficLight.c
+++ trafficLight.c
@@ -12,21 +12,27 @@
 #include <wiringPi.h>
 
 #include "trafficLight.h"
-#include "train.h"
-//static trafficLight_t* light;
+#include "lsquaredc.h"
+#include "daemon.h"
+// Random values, this will change to match the firmware of the barrier
+#define I2C_TRAFFIC_LIGHT_ADDRESS 0x28
+#define I2C_TRAFFIC_LIGHT_ON 0x29
+#define I2C_TRAFFIC_LIGHT_OFF 0x30
+
 trafficLight_t* trafficLights[MAXTRAFFICLIGHTS];
-int n_trafficLights;
 
-void trafficLight_setup(void) {
+int n_trafficLights = 0;
 
-	 //trafficLight_t* dummy = trafficLight_new(0, 12);
-	 trafficLight_new(0, 13);
-	 interp_addcmd("semaphore", trafficLight_cmd, "Set semaphore state\n");
-	 
-	 
+void 
+trafficLight_setup(void) 
+{
+	 trafficLight_new(0, I2C_TRAFFIC_LIGHT_ADDRESS);
+	 interp_addcmd("semaphore", trafficLight_cmd, "Set semaphore state\n");	 
 }
 
-int trafficLight_cmd(char* arg) {
+int 
+trafficLight_cmd(char* arg) 
+{
     int i;
 	if (0 == strcmp(arg, "list")) {
         for( i= 0; i<n_trafficLights;i++)
@@ -44,36 +50,61 @@ int trafficLight_cmd(char* arg) {
 	return 1;
 }
 
-void trafficLight_init(trafficLight_t* this, int id, int GPIOline,
-		state_t state) {
+void 
+trafficLight_init(trafficLight_t* this, int id, uint16_t i2c_address,
+		state_t state) 
+{
         observable_init(&this->observable);        
 	this->state = state;
-	this->GPIOline = GPIOline;
+	//this->GPIOline = GPIOline;
+	this->i2c_address = i2c_address;
 	rt_mutex_create(&this->mutex, NULL);
 
 	// Set GPIO line as output
-	pinMode(this->GPIOline, OUTPUT);
+	//pinMode(this->GPIOline, OUTPUT);
 	trafficLight_set_state(this, state);
 
 }
 
 trafficLight_t*
-trafficLight_new(int id, int GPIOline) {
+trafficLight_new(int id, uint16_t i2c_address) 
+{
 	trafficLight_t* this = (trafficLight_t*) malloc(sizeof(trafficLight_t));
-	trafficLight_init(this, id, GPIOline, OFF);
-    if (n_trafficLights < MAXTRAFFICLIGHTS) {
+	trafficLight_init(this, id, i2c_address, OFF);
+        if (n_trafficLights < MAXTRAFFICLIGHTS) {
 		trafficLights[n_trafficLights++] = this;
 	}
 	return this;
 
 }
 
-state_t trafficLight_get_state(trafficLight_t* this) {
-
+state_t 
+trafficLight_get_state(trafficLight_t* this) 
+{
 	return this->state;
-
 }
 
+void 
+trafficLight_set_state(trafficLight_t* this, state_t state) 
+{
+        uint16_t trafficLight_comand[]={(this->i2c_address<<1), 0x00};
+	
+        rt_mutex_acquire(&(this->mutex), TM_INFINITE);
+	this->state = state;
+	rt_mutex_release(&(this->mutex));
+	
+	if (state == ON){
+		trafficLight_comand[1]=I2C_TRAFFIC_LIGHT_ON;
+	}else{
+		trafficLight_comand[1]=I2C_TRAFFIC_LIGHT_OFF;
+        }	
+        rt_mutex_acquire(&(i2chandler[0]->mutex), TM_INFINITE);
+        i2c_send_sequence(i2chandler[0]->i2chandler, trafficLight_comand, 2, 0);
+        rt_mutex_release(&i2chandler[0]->mutex);
+	
+
+}
+/*
 void trafficLight_set_state(trafficLight_t* this, state_t state) {
 	rt_mutex_acquire(&(this->mutex), TM_INFINITE);
 	this->state = state;
@@ -81,7 +112,7 @@ void trafficLight_set_state(trafficLight_t* this, state_t state) {
 		digitalWrite(this->GPIOline, 1);
 	else
 		digitalWrite(this->GPIOline, 0);
-		
+
 	rt_mutex_release(&(this->mutex));
 
-}
+}*/
