9093339ea76740e8d66c5fb04b143afdd04f92eb
tomasvalno <tomasvalno@gmail.com>
2014-05-28 21:23:24 +0200

    Update semaphore.c

--- semaphore.c
+++ semaphore.c
@@ -2,19 +2,18 @@
 #include <stdlib.h>
 //#include <rtdk.h>
 #include "lsquaredc.h"
-
+#include "daemon.h"
 // Random values, this will change to match the firmware of the barrier
-#define I2C_SEMAPHORE_ADRESS_0 0x22
-#define I2C_SEMAPHORE_ADRESS_1 0x23
-#define I2C_SEMAPHORE_ADRESS_2 0x24
-#define I2C_SEMAPHORE_ADRESS_3 0x25
+#define I2C_SEMAPHORE_ADDRESS_0 0x22
+#define I2C_SEMAPHORE_ADDRESS_1 0x23
+#define I2C_SEMAPHORE_ADDRESS_2 0x24
+#define I2C_SEMAPHORE_ADDRESS_3 0x2
 #define I2C_SEMAPHORE_ON 0x00
 #define I2C_SEMAPHORE_OFF 0x01
 
 semaphore_t* semaphores[MAXSEMAFORES];
 int nsemaphores;
-static uint16_t i2c_addresses[]={I2C_SEMAPHORE_ADRESS_0,I2C_SEMAPHORE_ADRESS_1,
-				I2C_SEMAPHORE_ADRESS_2,I2C_SEMAPHORE_ADRESS_3};
+static uint16_t i2c_addresses[]={I2C_SEMAPHORE_ADDRESS_0,I2C_SEMAPHORE_ADDRESS_1,I2C_SEMAPHORE_ADDRESS_2,I2C_SEMAPHORE_ADDRESS_3};
 
 semaphore_t* 
 semaphore_new(int id, int state, uint16_t i2c_address)
@@ -63,27 +62,10 @@ semaphore_switch (semaphore_t* this, int state)
     } 
     rt_mutex_acquire(&this->mutex, TM_INFINITE);
     this->state = state;
-    i2c_send_sequence(i2c0handle, semaphore_comand, 2, 0);
     rt_mutex_release(&this->mutex);
-}
+        
+    rt_mutex_acquire(&(i2chandler[1]->mutex), TM_INFINITE);
+    i2c_send_sequence(i2chandler[1]->i2chandler, semaphore_comand, 2, 0);
+   	rt_mutex_release(&i2chandler[1]->mutex);
 
-/*
-void 
-semaphore_switch_on(int i)
-{
-    semaphore_t* local = semaphores[i];
-    rt_mutex_acquire(&local->mutex, TM_INFINITE);
-    local->state = 1;
-    rt_mutex_release(&local->mutex);
-    //rt_printf(" semafore %d switched to %d (ON)\n", i , semaphores[i]->state);
 }
-void 
-semaphore_switch_off(int i)
-{
-    semaphore_t* local = semaphores[i];
-    rt_mutex_acquire(&local->mutex, TM_INFINITE);
-    local->state = 0;
-    rt_mutex_release(&local->mutex);
-    //rt_printf(" semafore %d switched to %d (OFF)\n", i , semaphores[i]->state);                        
-}
-*/
