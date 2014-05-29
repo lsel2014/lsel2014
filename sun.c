d2f4ffb9a1c29270925bca87b653c36c02e6a7c9
tomasvalno <tomasvalno@gmail.com>
2014-05-29 19:06:03 +0200

    Update sun.c

--- sun.c
+++ sun.c
@@ -15,8 +15,8 @@
 sun_t* sun;
 
 void sun_setup(void) {
-	//sun = sun_new(DEFAULT_DATE, 0x20, SUN_DEADLINE);
-	sun = sun_new(DEFAULT_DATE, I2C_ADRESS_SUN , SUN_DEADLINE);
+	sun = sun_new(DEFAULT_DATE, 0x20, SUN_DEADLINE);
+	//sun = sun_new(DEFAULT_DATE, I2C_ADRESS_SUN , SUN_DEADLINE);
 	interp_addcmd("sun", sun_cmd, "Set sun parameters\n");
 }
 
