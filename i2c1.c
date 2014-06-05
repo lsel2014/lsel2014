5e41b32ef1e328dc00d46a27d1e53755d1135178
tomasvalno <tomasvalno@gmail.com>
2014-06-05 12:24:34 +0200

    Update i2c1.c

--- i2c1.c
+++ i2c1.c
@@ -10,11 +10,19 @@ i2csetup(void)
 bcm2835_init();
 bcm2835_gpio_fsel(0,BCM2835_FSEL_INPT);
 bcm2835_gpio_fsel(1,BCM2835_FSEL_INPT);
+bcm2835_gpio_fsel(2,BCM2835_FSEL_INPT);
+bcm2835_gpio_fsel(3,BCM2835_FSEL_INPT);
 bcm2835_gpio_fsel(28,BCM2835_FSEL_INPT);
 bcm2835_gpio_fsel(29,BCM2835_FSEL_INPT);
-
+// Set bus i2c 0
+bcm2835_gpio_fsel(2,BCM2835_FSEL_ALT0);
+bcm2835_gpio_set_pud(2,BCM2835_GPIO_PUD_UP);
+bcm2835_gpio_fsel(3,BCM2835_FSEL_ALT0);
+bcm2835_gpio_set_pud(3,BCM2835_GPIO_PUD_UP);
+// Set bus i2c 1
 bcm2835_gpio_fsel(28,BCM2835_FSEL_ALT0);
 bcm2835_gpio_set_pud(28,BCM2835_GPIO_PUD_UP);
 bcm2835_gpio_fsel(29,BCM2835_FSEL_ALT0);
 bcm2835_gpio_set_pud(29,BCM2835_GPIO_PUD_UP);
+
 }
