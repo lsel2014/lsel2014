a8cbd9a59940f985cdb62818d2e5c2f1e4533a92
tomasvalno <tomasvalno@gmail.com>
2014-06-18 12:06:49 +0200

    Update semaphore.c

--- semaphore.c
+++ semaphore.c
@@ -7,7 +7,8 @@
 
 semaphore_t* semaphores[MAXSEMAFORES];
 int nsemaphores;
-static uint16_t i2c_addresses[]={I2C_SEMAPHORE_ADDRESS_0,I2C_SEMAPHORE_ADDRESS_1,I2C_SEMAPHORE_ADDRESS_2,I2C_SEMAPHORE_ADDRESS_3};
+static uint16_t i2c_addresses[]={I2C_SEMAPHORE_ADDRESS_0,I2C_SEMAPHORE_ADDRESS_1,
+				I2C_SEMAPHORE_ADDRESS_2,I2C_SEMAPHORE_ADDRESS_3};
 
 semaphore_t* 
 semaphore_new(int id, uint16_t state, uint16_t i2c_address)
@@ -63,8 +64,8 @@ semaphore_switch (semaphore_t* this)
     uint16_t semaphore_comand[2]={(this->i2c_address<<1),semaphore_get_state(this)};
     
         
-    rt_mutex_acquire(&(i2chandler[1]->mutex), TM_INFINITE);
+    //rt_mutex_acquire(&(i2chandler[1]->mutex), TM_INFINITE);
     i2c_send_sequence(i2chandler[1]->i2chandler, semaphore_comand, 2, 0);
-    rt_mutex_release(&i2chandler[1]->mutex);
+    //rt_mutex_release(&i2chandler[1]->mutex);
 
 }
